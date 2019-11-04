8b6f3da4308e345ad2c40a952e52c8fcce428946
Kyle <kylepcole@gmail.com>
2019-11-04 15:02:35 -0500

    lab-work

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor
+	@@all = []
+
+	attr_reader :name
+
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def appointments
+		Appointment.all.select do |appointment|
+			appointment.doctor == self
+		end
+	end
+
+	def patients
+		self.appointments.map do |appointment|
+			appointment.patient
+		end
+	end
+
+	def new_appointment(patient, date)
+		Appointment.new(date, patient, self)
+	end
+end
